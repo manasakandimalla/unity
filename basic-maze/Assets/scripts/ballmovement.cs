3ef6e30cdb8947d602d3cbaca44ba9df2183c98f
manasakandimalla <33128014+manasakandimalla@users.noreply.github.com>
2018-02-26 21:45:32 +0530

    Initial commit

---
+++ basic-maze/Assets/scripts/ballmovement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ballmovement : MonoBehaviour
+{
+    public Text countText;
+    public Text winText;
+    public GameObject door;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * 100);
+        if((rb.position.x)>=500)
+        {
+            winText.text = "You Win!!!";
+            Destroy(rb);
+        }
+        if((rb.position.x)< -500)
+        {
+            //winText.text = "Can't get out from here";
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+                  
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Gem"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 10)
+        {
+            winText.text = "Door unlocked!";
+            Destroy(door.gameObject);
+
+        }
+    }
+}
---
+++ maze-ar/Assets/script/ballscrpit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballscrpit : MonoBehaviour
+{
+
+    public GameObject plane;
+    public GameObject sppnt;
+    public Rigidbody rb;
+
+    public float thrust;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (transform.position.y < plane.transform.position.y - 10)
+        {
+            Debug.Log("getting back to initial position");
+            rb.velocity = Vector3.zero;
+            transform.position = sppnt.transform.position;
+
+        }
+
+    }
+}
